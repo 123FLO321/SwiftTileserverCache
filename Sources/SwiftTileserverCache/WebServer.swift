2cf2f32f9e42d1eeb44f924ef5c125f3ac79217b
Florian Kostenzer <florian.kostenzer@icloud.com>
2019-11-05 15:21:44 +0100

    set lang to english

--- Sources/SwiftTileserverCache/WebServer.swift
+++ Sources/SwiftTileserverCache/WebServer.swift
@@ -237,7 +237,7 @@ public class WebServer {
         
         let html = """
         <!DOCTYPE html>
-        <html lang="de">
+        <html lang="en">
         <head>
             <meta charset="utf-8"/>
             <title>SwiftTileserver Cache</title>
