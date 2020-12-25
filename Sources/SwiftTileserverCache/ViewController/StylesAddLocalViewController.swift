5dfd05f5ba9bb1d45202f586a2eab7e11a6cf069
Florian Kostenzer <florian.kostenzer@icloud.com>
2020-12-25 12:21:53 +0100

    Add Maputnik link

--- Sources/SwiftTileserverCache/ViewController/StylesAddLocalViewController.swift
+++ Sources/SwiftTileserverCache/ViewController/StylesAddLocalViewController.swift
@@ -7,14 +7,24 @@ internal class StylesAddLocalViewController: ViewController {
     internal struct Context: ViewControllerContext {
         var pageId: String
         var pageName: String
+        var previewLatitude: Double
+        var previewLongitude: Double
     }
 
-    init() {}
+    let previewLatitude: Double
+    let previewLongitude: Double
+
+    init() {
+        self.previewLatitude = Double(Environment.get("PREVIEW_LATIDUDE") ?? "") ?? 47.377105
+        self.previewLongitude = Double(Environment.get("PREVIEW_LONGITUDE") ?? "") ?? 8.541655
+    }
 
     internal func render(request: Request) -> EventLoopFuture<View> {
         let context = Context(
             pageId: "styles",
-            pageName: "Add Local Style"
+            pageName: "Add Local Style",
+            previewLatitude: self.previewLatitude,
+            previewLongitude: self.previewLongitude
         )
         return self.render(request: request, template: "StylesAddLocal", context: context)
     }
